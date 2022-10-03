38e862f3c820c6ef874167f28ecd4dd158bc2701
Aleksandr <Aleksander.n.gusev@mail.ru>
2022-10-03 22:11:41 +0300

    add: file example006

---
+++ Example006_Masha/Program.cs
@@ -0,0 +1,17 @@
+﻿
+
+
+Console.Write("Введите имя пользователя: ");
+
+string username = Console.ReadLine();
+
+if (username.ToLower() == "маша")  //ToLower ---- позволяет все символы вашей строки перевести в нижний регистр
+{
+    Console.Write("уРА, ");
+    Console.Write(username);
+}
+else 
+{
+    Console.Write("Здорова Отец, ");
+    Console.Write(username);
+}
