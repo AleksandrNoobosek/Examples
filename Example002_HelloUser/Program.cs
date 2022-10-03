ffbbaaa4d1243a46fb0a102ae94a14cd3a8c82bf
Aleksandr <Aleksander.n.gusev@mail.ru>
2022-10-03 19:58:19 +0300

    feat: создали папку HelloUser

---
+++ Example002_HelloUser/Program.cs
@@ -0,0 +1,13 @@
+﻿// Console.WriteLine вывести в новую строку
+// Console.ReadLine ввод переменной
+// string - строка, своего рода контейнер обьема памяти Пример :string username = Console.ReadLine (); ------без("")
+// string username = Console.ReadLine (""); -команда для ввода данных в строке
+// Console.WriteLine(username); вывод введенной переменной (без"")
+// Console.WriteLine("Введите ваше имя "); -пробел после текста и закрываю скобки, новвроде без этого тоже работает прога
+//Console.Write("Привет, "); Console.Write("Привет, "); ----вывод в одну строку
+// (//) - комментарии 
+
+Console.WriteLine("Введите ваше имя ");
+string username = Console.ReadLine();
+Console.Write("Привет, ");
+Console.Write(username);
