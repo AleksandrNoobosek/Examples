558a2c3903ff7c4c2c6ccf325ea0376fcb901257
Aleksandr <Aleksander.n.gusev@mail.ru>
2022-10-03 20:12:39 +0300

    add:example003_sum

---
+++ Example003_Sum/Program.cs
@@ -0,0 +1,17 @@
+﻿// Console.WriteLine вывести в новую строку
+// Console.ReadLine ввод переменной
+// string - строка, своего рода контейнер обьема памяти Пример :string username = Console.ReadLine (); ------без("")
+// string username = Console.ReadLine (""); -команда для ввода данных в строке
+// Console.WriteLine(username); вывод введенной переменной (без"")
+// Console.WriteLine("Введите ваше имя "); -пробел после текста и закрываю скобки, новвроде без этого тоже работает прога
+//Console.Write("Привет, "); Console.Write("Привет, "); ----вывод в одну строку
+// (//) - комментарии 
+// int -для хранения целых чисел
+// NumberA --при наисенование цифра не должна стоять на первом месте
+// int result переменная которой присвоили сумму (int result = NumberA+NumberB;)
+
+
+int NumberA = 13;
+int NumberB = 5;
+int result = NumberA+NumberB;
+Console.WriteLine(result);
