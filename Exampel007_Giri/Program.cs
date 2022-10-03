c5c0518964b95976bec187c3be6f7a5903ad9127
Aleksandr <Aleksander.n.gusev@mail.ru>
2022-10-03 22:34:41 +0300

    add: file example007_giri

---
+++ Exampel007_Giri/Program.cs
@@ -0,0 +1,33 @@
+﻿// задача - гири(определить максимальный вес гири)
+int a = new Random().Next(1,100);
+int b = new Random().Next(1,100);
+int c = new Random().Next(1,100);
+int d = new Random().Next(1,100);
+int e = new Random().Next(1,100);
+
+Console.WriteLine(a);
+Console.WriteLine(b);
+Console.WriteLine(c);
+Console.WriteLine(d);
+Console.WriteLine(e);
+
+int max = a;
+
+if (b>max)
+{
+        max=b;
+}
+if (c>max)
+{
+        max=c;
+}
+if (d>max)
+{
+        max=d;
+}
+if (e>max)
+{
+        max=e;
+}
+Console.Write("max = ");
+Console.WriteLine(max);
