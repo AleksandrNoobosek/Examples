8b87224e642ab76478233c40fb27344ddd3858e0
Aleksandr <Aleksander.n.gusev@mail.ru>
2022-10-06 20:44:46 +0300

    add110

---
+++ Example110_Sum_F/Program.cs
@@ -0,0 +1,30 @@
+﻿
+
+
+int Max(int arg1,int arg2,int arg3)
+{
+	int result = arg1;
+	if(arg2>result) result = arg2;
+	if(arg3>result) result = arg3;
+	return result;
+}
+
+int a1 = 13;
+int a2 = 34;
+int a3 = 1234;
+int b1 = 23;
+int b2 = 11;
+int b3 = 912345;
+int c1 = 1;
+int c2 = 8;
+int c3 = 90;
+
+int max1 = Max(a1,b1,c1);
+int max2 = Max(a2,b2,c2);
+int max3 = Max(a3,b3,c3);
+int max = Max(max1,max2,max3);
+
+Console.WriteLine(max);
+
+
+
